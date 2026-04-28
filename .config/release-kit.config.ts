aa91934293c747b55ec8006be3a5d76da0b9dc7a
William Thorsen <william@thorsen.dev>
2026-04-28 13:40:54 -0700

    root|deps: Upgrade release-kit to v5

---
+++ .config/release-kit.config.ts
@@ -0,0 +1,9 @@
+import type { ReleaseKitConfig } from '@williamthorsen/release-kit';
+
+const config: ReleaseKitConfig = {
+  releaseNotes: {
+    shouldInjectIntoReadme: true,
+  },
+};
+
+export default config;
